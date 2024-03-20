926dea5d16232ab2d1d01a5d57e02941dbdee7fb
hanbit10 <chanbit10@gmail.com>
2024-03-20 10:53:53 +0100

    Basic Structure updated 20.03 Hanbit Chang

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,41 +1,31 @@
-function includeHTMLSnippet() {
-  // Traverse the collection of all
-  // HTML elements
-  id = document.getElementsByTagName("*");
-  for (i = 0; i < id.length; i++) {
-    element = id[i];
-
-    // Search for elements with
-    // specific attributes
-    file = element.getAttribute("include-html-snippet");
-
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
     if (file) {
-      // Create an HTTP request with
-      // the attribute value as the
-      // file name
-      xmlRequest = new XMLHttpRequest();
-      xmlRequest.onreadystatechange = function () {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
           if (this.status == 200) {
-            element.innerHTML = this.responseText;
+            elmnt.innerHTML = this.responseText;
           }
-
           if (this.status == 404) {
-            element.innerHTML = "Page not found.";
+            elmnt.innerHTML = "Page not found.";
           }
-
-          // Delete the attribute and
-          // call this function again.
-          element.removeAttribute("include-html-snippet");
-
-          includeHTMLSnippet();
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
         }
       };
-      xmlRequest.open("GET", file, true);
-      xmlRequest.send();
-      return; // Exit function.
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
     }
   }
 }
-
-includeHTMLSnippet();
